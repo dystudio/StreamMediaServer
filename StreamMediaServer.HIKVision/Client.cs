cf99ac6fdc9a861b636d4cf88fcc7e79c557ee28
Wanlong Liu <wanlong1003@163.com>
2018-04-18 01:16:12 +0800

    推流修改为异步方式，异步模式不阻断线程

--- StreamMediaServer.HIKVision/Client.cs
+++ StreamMediaServer.HIKVision/Client.cs
@@ -10,6 +10,7 @@ namespace StreamMediaServer.HIKVision
 {
     public static class Client
     {
+        private static List<VideoModel> _videos = new List<VideoModel>();
         /// <summary>
         /// 初始化
         /// </summary>
@@ -52,101 +53,127 @@ namespace StreamMediaServer.HIKVision
         {
             return Task.Run(() =>
             {
-                int inx = 0;
-                var rtmpId = -1;
-                List<byte> buffer = new List<byte>();
-                NET_DVR_DEVICEINFO_V30 deviceInfo = new NET_DVR_DEVICEINFO_V30();
-                int playerId = -1;
+                var _video = new VideoModel()
+                {
+                    IP = ip,
+                    Port = port,
+                    Channel = 1
+                };
+
+                //取得推流句柄
+                _video.RTMPHandle = LKRtmp.LKRtmp_Init(rtmp);
+                if (_video.RTMPHandle <= 0)
+                {
+                    Console.WriteLine("取得推流句柄失败");
+                    return;
+                }
 
                 //同步登陆
-                var userid = CHCNetSDK.NET_DVR_Login_V30(ip, port, username, password, ref deviceInfo);
-                if (userid >= 0)
-                {
-                    var sn = Encoding.UTF8.GetString(deviceInfo.sSerialNumber);
-                    Console.WriteLine("SN: " + sn);
+                NET_DVR_DEVICEINFO_V30 deviceInfo = new NET_DVR_DEVICEINFO_V30();
+                _video.UserHandle = CHCNetSDK.NET_DVR_Login_V30(ip, port, username, password, ref deviceInfo);
 
-                    var previewInfo = new NET_DVR_PREVIEWINFO();
-                    previewInfo.hPlayWnd = IntPtr.Zero;//预览窗口 live view window
-                    previewInfo.lChannel = 1;//预览的设备通道 the device channel number
-                    previewInfo.dwStreamType = 0;//码流类型：0-主码流，1-子码流，2-码流3，3-码流4，以此类推
-                    previewInfo.dwLinkMode = 0;//连接方式：0- TCP方式，1- UDP方式，2- 多播方式，3- RTP方式，4-RTP/RTSP，5-RSTP/HTTP 
-                    previewInfo.bBlocked = true; //0- 非阻塞取流，1- 阻塞取流
-                    previewInfo.dwDisplayBufNum = 15; //播放库显示缓冲区最大帧数
-
-                    //开视播放                   
-                    playerId = CHCNetSDK.NET_DVR_RealPlay_V40(userid
-                        , ref previewInfo
-                        , (lRealHandle, dwDataType, pBuffer, dwBufSize, pUser) =>
-                        {
-                            if (dwDataType == 1)  //数据头
-                            {
-                                rtmpId = LKRtmp.LKRtmp_Init(rtmp);
-                                Console.WriteLine("链接结果：" + rtmpId);
-                            }
-                            else if (dwDataType == 2)
-                            {
-                                var head = Marshal.ReadInt32(pBuffer, 0);
-                                if (head == -1174339584) //0x00, 0x00, 0x01, 0xBA = -1174339584   PS包
-                                {
-                                    /*如果收到PS包头，表示的是接下来的数据是一个新的PS包，上一个PS数据包接收完成*/
-                                    if (buffer.Count > 0)
-                                    {
-                                        //Marshal.AllocHGlobal 分配内存 使用完成后一定要使用Marshal.FreeHGlobal 释放，否则会造成内存泄漏
-                                        IntPtr p = Marshal.AllocHGlobal(buffer.Count);
-                                        Marshal.Copy(buffer.ToArray(), 0, p, buffer.Count);
-                                        var ts = (ulong)((DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000);
-                                        var result = LKRtmp.LKRtmp_PutData(rtmpId, 100, p, buffer.Count, ts, buffer[4] == 0x67 ? 1 : 0);
-                                        Marshal.FreeHGlobal(p);
-                                    }
-                                    buffer.Clear();
-                                }
-                                else if (-536805376 == head) //-536805376 = 0x00, 0x00, 0x01, 0xE0   视频
-                                {
-                                    /*如果收到视频包则追加到缓存中*/
-                                    var skip = Marshal.ReadByte(pBuffer, 8) + 9;
-                                    var buff = new byte[dwBufSize - skip];
-                                    var pData = IntPtr.Add(pBuffer, skip);
-                                    Marshal.Copy(pData, buff, 0, (int)(dwBufSize - skip));
-                                    buffer.AddRange(buff);
-                                }
-                                else if (-1073676288 == head)   //-1073676288 = 0x00, 0x00, 0x01, 0xC0  音频
-                                {
-                                    /*如果收到音频包则追加到缓存中*/
-                                    var buff = new byte[dwBufSize];
-                                    Marshal.Copy(pBuffer, buff, 0, (int)dwBufSize);
-                                    File.WriteAllBytes(inx++.ToString("000000"), buff);
-                                }
-                                else
-                                {
-                                    //var t = BitConverter.GetBytes(head);
-                                    //Console.WriteLine($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] 未知包头:{t[0].ToString("X")} {t[1].ToString("X")} {t[2].ToString("X")} {t[3].ToString("X")}");
-                                }
-                            }
-                        }
-                        , IntPtr.Zero);
-
-
-                    if (playerId < 0)
-                    {
-                        Console.WriteLine("预览失败，错误码：" + CHCNetSDK.NET_DVR_GetLastError());
-                    }
-                }
-                else
+                if (_video.UserHandle < 0)
                 {
                     Console.WriteLine("登陆失败，错误码：" + CHCNetSDK.NET_DVR_GetLastError());
-                }
-                Console.ReadLine();
-                if (playerId >= 0)
-                {
-                    CHCNetSDK.NET_DVR_StopRealPlay(playerId);
-                    Console.WriteLine("停止预览");
+                    LKRtmp.LKRtmp_Fini(_video.RTMPHandle);
+                    return;
                 }
 
-                if (userid >= 0)
+                var sn = Encoding.UTF8.GetString(deviceInfo.sSerialNumber);
+                Console.WriteLine($"IP:{ip}, SN: {sn}");
+
+                var previewInfo = new NET_DVR_PREVIEWINFO
                 {
-                    CHCNetSDK.NET_DVR_Logout_V30(userid);
-                    Console.WriteLine("登出");
+                    hPlayWnd = IntPtr.Zero,//预览窗口 live view window
+                    lChannel = _video.Channel,//预览的设备通道 the device channel number
+                    dwStreamType = 0,//码流类型：0-主码流，1-子码流，2-码流3，3-码流4，以此类推
+                    dwLinkMode = 0,//连接方式：0- TCP方式，1- UDP方式，2- 多播方式，3- RTP方式，4-RTP/RTSP，5-RSTP/HTTP 
+                    bBlocked = true, //0- 非阻塞取流，1- 阻塞取流
+                    dwDisplayBufNum = 15 //播放库显示缓冲区最大帧数
+                };
+
+                //开视播放                   
+                _video.RealHandle = CHCNetSDK.NET_DVR_RealPlay_V40(_video.UserHandle
+                    , ref previewInfo
+                    , (iRealHandle, dwDataType, pBuffer, dwBufSize, pUser) =>
+                    {
+                        var video = _videos.Find(d => d.RealHandle == iRealHandle);
+                        if (video == null)
+                        {
+                            CHCNetSDK.NET_DVR_StopRealPlay(iRealHandle);
+                            return;
+                        }
+
+                        //lRealHandle和CHCNetSDK.NET_DVR_RealPlay_V40返回值是一样的
+                        if (dwDataType == 1)  //数据头
+                        {
+                            //海康私有协议头，只有第一次调用是为1                            
+                        }
+                        else if (dwDataType == 2)
+                        {
+                            var head = Marshal.ReadInt32(pBuffer, 0);
+                            if (head == -1174339584) //0x00, 0x00, 0x01, 0xBA = -1174339584   PS包
+                            {
+                                /*如果收到PS包头，表示的是接下来的数据是一个新的PS包，上一个PS数据包接收完成*/
+
+                                if (video.Buffer.Count > 0)
+                                {
+                                    //Marshal.AllocHGlobal 分配内存 使用完成后一定要使用Marshal.FreeHGlobal 释放，否则会造成内存泄漏
+                                    var arr = new byte[video.Buffer.Count];
+                                    Array.Copy(video.Buffer.ToArray(), arr, video.Buffer.Count);
+                                    var tuple = new Tuple<int, byte[]>(video.RTMPHandle, arr);
+
+                                    //异步推流
+                                    Task.Factory.StartNew((obj)=> {
+                                        var v = obj as Tuple<int, byte[]>;
+                                        IntPtr p = Marshal.AllocHGlobal(v.Item2.Length);
+                                        Marshal.Copy(v.Item2, 0, p, v.Item2.Length);
+                                        var ts = (ulong)((DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000);
+                                        var result = LKRtmp.LKRtmp_PutData(v.Item1, 100, p, v.Item2.Length, ts, v.Item2[4] == 0x67 ? 1 : 0);
+                                        if(result < 0)
+                                        {
+                                            Console.WriteLine("推流结果="+result);
+                                        }
+                                        Marshal.FreeHGlobal(p);
+                                    }, tuple);
+                                }
+                                video.Buffer.Clear();
+                            }
+                            else if (-536805376 == head) //-536805376 = 0x00, 0x00, 0x01, 0xE0   视频
+                            {
+                                /*如果收到视频包则追加到缓存中*/
+                                var skip = Marshal.ReadByte(pBuffer, 8) + 9;
+                                var buff = new byte[dwBufSize - skip];
+                                var pData = IntPtr.Add(pBuffer, skip);
+                                Marshal.Copy(pData, buff, 0, (int)(dwBufSize - skip));
+                                video.Buffer.AddRange(buff);
+                            }
+                            else if (-1073676288 == head)   //-1073676288 = 0x00, 0x00, 0x01, 0xC0  音频
+                            {
+                                /*如果收到音频包则追加到缓存中*/
+                                var buff = new byte[dwBufSize];
+                                Marshal.Copy(pBuffer, buff, 0, (int)dwBufSize);
+                                //File.WriteAllBytes(inx++.ToString("000000"), buff);
+                            }
+                            else
+                            {
+                                //var t = BitConverter.GetBytes(head);
+                                //Console.WriteLine($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] 未知包头:{t[0].ToString("X")} {t[1].ToString("X")} {t[2].ToString("X")} {t[3].ToString("X")}");
+                            }
+                        }
+                    }
+                    , IntPtr.Zero);
+
+
+                if (_video.RealHandle < 0)
+                {
+                    Console.WriteLine("预览失败，错误码：" + CHCNetSDK.NET_DVR_GetLastError());
+                    LKRtmp.LKRtmp_Fini(_video.RTMPHandle);
+                    CHCNetSDK.NET_DVR_Logout_V30(_video.UserHandle);
+                    return;
                 }
+
+                _videos.Add(_video);
             });
         }
     }
---
+++ StreamMediaServer.HIKVision/VideoModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreamMediaServer.HIKVision
+{
+    class VideoModel
+    {
+        public VideoModel()
+        {
+            Buffer = new List<byte>();
+        }
+
+        public string IP
+        {
+            get; set;
+        }
+
+        public int Port
+        {
+            get; set;
+        }
+
+        public int Channel
+        {
+            get; set;
+        }
+
+        public int UserHandle
+        {
+            get; set;
+        }
+
+        public int RealHandle
+        {
+            get;set;
+        }
+
+        public int RTMPHandle
+        {
+            get; set;
+        }
+
+        public List<byte> Buffer
+        {
+            get;set;
+        }
+    }
+}
